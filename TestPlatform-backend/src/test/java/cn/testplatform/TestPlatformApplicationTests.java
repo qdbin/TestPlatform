afb399f49871f56f42fea77464156818da889452
qdbin <3115231936@qq.com>
2025-09-24 02:06:10 +0800

    后端初始化

---
+++ TestPlatform-backend/src/test/java/cn/testplatform/TestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.testplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
