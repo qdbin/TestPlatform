b99df44e35900a8de67a78502f9e23bd927b0e36
qdbin <3115231936@qq.com>
2025-10-05 02:58:38 +0800

    分页vue组件

---
+++ platform-frontend/src/views/common/components/pagination.vue
@@ -0,0 +1,40 @@
+/**
+* 分页组件
+*/ 
+<template>
+  <el-pagination class="page-box" @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="childMsg.currentPage" :page-sizes="[10, 20, 50, 100]" :page-size="childMsg.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="childMsg.total">
+  </el-pagination>
+</template>
+<script>
+export default {
+  name: 'Pagination',
+  props: ['childMsg'],
+  data() {
+    return {
+      pageparm: {
+        currentPage: this.childMsg.currentPage,
+        pageSize: this.childMsg.pageSize
+      }
+    }
+  },
+  created() {},
+  methods: {
+    // 改变pageSize
+    handleSizeChange(val) {
+      this.pageparm.pageSize = val
+      this.$emit('callFather', this.pageparm)
+    },
+    // 改变pageNum
+    handleCurrentChange(val) {
+      this.pageparm.currentPage = val
+      this.$emit('callFather', this.pageparm)
+    }
+  }
+}
+</script>
+
+<style>
+.page-box {
+  margin: 10px auto;
+}
+</style>
